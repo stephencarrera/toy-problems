fe51903e52b301cbe66968179851e37cee81ff8e
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-03 10:18:22 -0700

    complete 8KYU exercise

---
+++ JavaScript/8KYU/8KYU-Even-or-Odd.js
@@ -0,0 +1,14 @@
+/*
+
+https://www.codewars.com/kata/even-or-odd
+
+Instructions: 
+
+Create a function that takes an integer as an argument 
+and returns "Even" for even numbers or "Odd" for odd numbers.
+
+Refactored Solution: 
+
+*/
+
+const even_or_odd = num => (num % 2 ? "Odd" : "Even");
