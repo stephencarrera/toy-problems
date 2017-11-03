0e0aacd120a6f4a48f9b61443f0624f9e46ddd25
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-03 10:05:55 -0700

    complete 8KYU exercise

---
+++ JavaScript/8KYU/8KYU-Multiply.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/multiply/javascript
+
+// Instructions:
+
+// The code does not execute properly. Try to figure out why.
+
+// Solution:
+
+const multiply = (a, b) => a * b;
