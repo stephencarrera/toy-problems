cfc6c787510e6510d50af668f7be07daaaeedde0
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-04 10:10:40 -0700

    complete 8KYU exercise

---
+++ JavaScript/8KYU/8KYU-sum-without-highest-and-lowest-number.js
@@ -0,0 +1,33 @@
+/*
+
+https://www.codewars.com/kata/sum-without-highest-and-lowest-number
+
+Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+Example:
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+
+If array is empty, null or None, or if only 1 Element exists, return 0.
+Note:In C++ instead null an empty vector is used. In C there is no null. ;-) 
+
+
+-- There's no null in Haskell, therefore Maybe [Int] is used. Nothing represents null.
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have created other katas. Have a look if you like coding and challenges.
+
+
+
+*/
+
+function sumArray(array) {
+  array = array || [];
+  return array
+    .sort((a, b) => a - b)
+    .slice(1, -1)
+    .reduce((sum, num) => sum + num, 0);
+}
