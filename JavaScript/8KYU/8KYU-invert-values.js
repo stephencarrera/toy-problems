2bb385e4c9e4dba09d6fdcc01dc419c89df85923
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-04 10:32:58 -0700

    complete 8KYU exercise

---
+++ JavaScript/8KYU/8KYU-invert-values.js
@@ -0,0 +1,14 @@
+/*
+
+https://www.codewars.com/kata/invert-values
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers.
+
+*/
+
+const invert = array => array.map(num => 0 - num);
